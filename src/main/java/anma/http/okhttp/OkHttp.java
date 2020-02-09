165e6682e9ceb0e4c0500d1569360269b60608c6
andriim <quadr988@gmail.com>
2020-02-09 18:39:08 +0200

    okhttp added

--- src/main/java/anma/http/okhttp/OkHttp.java
+++ src/main/java/anma/http/okhttp/OkHttp.java
@@ -1,4 +1,47 @@
 package anma.http.okhttp;
 
+import anma.http.unirest.Planet;
+import com.google.gson.Gson;
+import okhttp3.ConnectionSpec;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 public class OkHttp {
+
+    static final String URL = "https://swapi.co/api/planets/1/";
+
+    static OkHttpClient getClient() {
+        return new OkHttpClient.Builder()
+                .connectionSpecs(Arrays.asList(ConnectionSpec.MODERN_TLS, ConnectionSpec.COMPATIBLE_TLS))
+                .build();
+    }
+
+    static String run (String url) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(url).build();
+
+        Response response = client.newCall(request).execute();
+
+        return response.body().string();
+
+    }
+
+    public static void main(String[] args) {
+
+        Gson gson = new Gson();
+
+        try {
+            System.out.println(gson.fromJson(run(URL), Planet.class));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
