278a0934160e874a94078670b31dbf72d2e0c3b7
andriim <quadr988@gmail.com>
2020-02-09 18:12:53 +0200

    okhttp added

--- src/main/java/anma/http/unirest/UnirestHttp.java
+++ src/main/java/anma/http/unirest/UnirestHttp.java
@@ -6,29 +6,35 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class UnirestHttp {
 
     private static String URL = "https://swapi.co/api/planets/1/";
-
     static String testJson = "{\"name\":\"Vasyl\", \"age\":\"56\"}";
-
     static String[] cats = {"Murzik", "Liza", "Bagira"};
 
-    public static void main(String[] args) throws UnirestException {
-
+    public static void main(String[] args) throws UnirestException, IOException, JSONException {
 //        Map<String, Object> map = gson.fromJson(json, new TypeToken<Map<String, Object>>() {}.getType());
 
         HttpResponse<JsonNode> response = Unirest.get(URL).asJson();
 
-        Planet planet = (Planet) response.getBody();
+        // Get object directly
 
-//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//        Person vasyl = gson.fromJson(testJson, Person.class);
-//        Planet planet = gson.fromJson(URL, Planet.class);
+        JSONObject jsonObject = response.getBody().getObject();
+        String diameter = jsonObject.getString("diameter");
+        System.out.println(diameter);
 
-        System.out.println("==========");
-        System.out.println();
+        // Get Object with GSON
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String responseBody = response.getBody().toString();
+        Planet planet = gson.fromJson(responseBody, Planet.class);
+        System.out.println(planet.getClimate());
+
 
     }
 }
